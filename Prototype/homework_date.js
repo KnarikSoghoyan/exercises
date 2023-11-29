dc14786c43e9a1c46a5f1215cb7fecc8deddd5f5
KnarikSoghoyan <knariksoghoyan@gmail.com>
2023-11-30 00:38:37 +0400

    changes in homework

--- Prototype/homework_date.js
+++ Prototype/homework_date.js
@@ -2,33 +2,44 @@
 // With moment npm package
 // With native Date
 
+const fs = require ('fs');
+
 let date1 = '01-01-2023'
-let date2 = '01-04-2023'
+let date2 = '12-31-2023'
 
 function allDates(startDate, endDate) {
     let start = new Date(startDate);
     let end = new Date(endDate);
 
     try {
-        if ((start.toString === "Invalid Date") || (end.toString === "Invalid Date")) {
+        if ((start.toString() === "Invalid Date")) {
             throw new Error("startDate is invalid")
         }
+        if ((end.toString() === "Invalid Date")) {
+            throw new Error("endDate is invalid")
+        }
+        let str = ''
+
+        while (start < end) {
+       
+            let year = start.getFullYear();
+            let month = start.getMonth()+1;
+            let day = start.getDate();
+            let hour = start.getHours();
+    
+            hour ++;
+            start.setHours(hour)
+    
+            str += `(year = ${year}, month = ${month}, day = ${day}, hour = ${hour})\n`
+        }
+        return str
 
     } catch (error) {
         console.log(error.message);
         return { message: "Please enter correct date" }
     }
+};
 
-    let year = start.getFullYear();
-    let month = start.getMonth() + 1;
-    let day = start.getDate();
-    let hour = start.getDate();
+const result = allDates(date1, date2);
 
-    while (start < end) {
-        console.log(`(year = ${year}, month = ${month}, day = ${day}, hour = ${hour})`);
-        hour++;
-        start.setHours(hour)
-    }
-
-}
-console.log(allDates(date1, date2));
+fs.writeFileSync ('myDates.json', result)
