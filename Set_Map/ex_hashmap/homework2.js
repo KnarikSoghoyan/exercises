ea145d6efd90c0f5cfb4a93e44e958aec70c146d
KnarikSoghoyan <knariksoghoyan@gmail.com>
2023-12-26 09:33:31 +0400

    homework2

---
+++ Set_Map/ex_hashmap/homework2.js
@@ -0,0 +1,32 @@
+// Given array of Objects. If each Domain-Publisher has 3 consecutive hours with less than 1000 pub 
+// imps per hour. (Given data is  by hour (see dateStamp))
+// If you need to check date go to  https://www.epochconverter.com/ and add there Datestamp +  00.
+// You need to return all values and instead of Datestmp return Date (you can convert datestmp 
+// to regular data in moment (format yyyy-dd-mm)) (edited) 
+
+const data = require('./data.json')
+const { convertDateStamp } = require('./convertDate.js')
+
+const domainPublisher = {}
+
+for (let i = 0; i < data.length-2; i++) {
+    let e = data[i]
+
+    e.DateStamp = convertDateStamp(e.DateStamp)
+
+    const key = `${e.Domain} - ${e.Publisher}`
+
+    newDate = new Date (e.date).getHours()
+    date1 = data[i].date,
+    date2 = data[i+1].date,
+    date3 = data[i+2].date
+   
+
+    if (e.PubImps > 1000) {
+        domainPublisher[key] ? domainPublisher[key].push(e) : domainPublisher[key] = [e]
+    }
+}
+console.log(domainPublisher);
+// console.log(Object.keys(domainPublisher).length);   
+
+
